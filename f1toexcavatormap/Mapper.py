02498e4885b602a1a0a7adafec8ed84b1617e391
Arran France <arranf@gmail.com>
2016-10-11 16:12:49 +0100

    Add test for Mapper.create_file and move individual_headers constant

--- f1toexcavatormap/Mapper.py
+++ f1toexcavatormap/Mapper.py
@@ -1,37 +1,7 @@
 import csv
+import os
+from f1toexcavatormap import Final
 from pathlib import Path
-
-individual_headers = ['FamilyId',
-                      'FamilyName',
-                      'CreatedDate',
-                      'PersonId',
-                      'Prefix',
-                      'FirstName',
-                      'NickName',
-                      'MiddleName',
-                      'LastName',
-                      'Suffix',
-                      'FamilyRole',
-                      'MaritalStatus',
-                      'ConnectionStatus',
-                      'RecordStatus',
-                      'IsDeceased',
-                      'HomePhone',
-                      'MobilePhone',
-                      'WorkPhone',
-                      'SMS Allowed?',
-                      'Email',
-                      'IsEmailActive',
-                      'Allow Bulk Email?',
-                      'Gender',
-                      'DateOfBirth',
-                      'School',
-                      'GraduationDate',
-                      'AnniversaryDate',
-                      'GeneralNote',
-                      'MedicalNote',
-                      'SecurityNote',
-                      ]
 
 
 def check_headers_exist(filename):
@@ -43,12 +13,19 @@ def check_headers_match(filename):
         with open(filename, 'rb') as file:
             reader = csv.reader(file)
             reader.next()
-            return reader.fieldnames == individual_headers
+            return reader.fieldnames == Final.individual_headers
 
 
 def check_file_exists(file_path):
     file = Path(file_path)
     return file.is_file()
+
+
+def touch(file_name, mode=0o666, dir_fd=None, **kwargs):
+    flags = os.O_CREAT | os.O_APPEND
+    with os.fdopen(os.open(file_name, flags=flags, mode=mode, dir_fd=dir_fd)) as f:
+        os.utime(f.fileno() if os.utime in os.supports_fd else file_name,
+                 dir_fd=None if os.supports_fd else dir_fd, **kwargs)
 
 
 def write_to_csv(filename, fields):
--- tests/CSVTests.py
+++ tests/CSVTests.py
@@ -8,12 +8,20 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 class CSVTests(unittest.TestCase):
     def setUp(self):
         self.test_file_path = os.path.join(THIS_DIR, 'testdata.csv')
-        os.remove(self.test_file_path)
+        try:
+            os.remove(self.test_file_path)
+        except Exception:
+            pass
 
     # File will have been removed by setUp
     def test_file_does_not_exist(self):
         self.assertFalse(mapper.check_file_exists(self.test_file_path))
 
+    def test_create_file_exists(self):
+        mapper.touch("testdata.csv")
+        self.assertTrue(mapper.check_file_exists(self.test_file_path))
+
+    # def test_create_file_has_headers(self):
 
 if __name__ == '__main__':
     unittest.main()
