1299697d07ca3ca363e04ba3de3ce97e593e2a85
Arran France <arranf@gmail.com>
2016-11-07 22:20:44 +0000

    Add financial tests

--- tests/FinancialBuilderTests.py
+++ tests/FinancialBuilderTests.py
@@ -8,6 +8,53 @@ class FinancialBuilderTests(unittest.TestCase):
     def test_batches_all_have_unique_ids(self):
         pass
 
+    def test_batches_ids_are_all_ints(self):
+        pass
+
+    def test_all_batches_have_names(self):
+        pass
+
+    def test_batches_all_have_amounts(self):
+        pass
+
+    def test_batch_amounts_are_all_decimal(self):
+        pass
+
+    def test_batch_ids_are_all_unique(self):
+        pass
+
+    def test_sum_data_amounts_equal_sum_batch_amounts(self):
+        pass
+
+    def test_batch_columns_match(self):
+        pass
+
+    def test_all_contributions_have_funds(self):
+        pass
+
+    def test_all_contributions_have_gls(self):
+        pass
+
+    def test_all_contributions_have_an_active_value(self):
+        pass
+
+    def test_all_contributions_received_dates_are_dates(self):
+        pass
+
+    def test_all_check_contributions_have_check_number(self):
+        pass
+
+    def test_contributions_amount_equals_contribution_amount(self):
+        pass
+
+    def test_all_contribution_batch_ids_have_batches(self):
+        pass
+
+    def test_all_contribution_ids_are_unique(self):
+        pass
+
+    def test_contributions_columns_match(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
